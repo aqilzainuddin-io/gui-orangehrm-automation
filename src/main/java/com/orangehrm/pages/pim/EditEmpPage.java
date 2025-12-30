f0f6adda96bed0b8b757e51429021eeb7734d266
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-12-30 08:35:29 +0800

    chore: add comments for editemppage class

--- src/main/java/com/orangehrm/pages/pim/EditEmpPage.java
+++ src/main/java/com/orangehrm/pages/pim/EditEmpPage.java
@@ -10,13 +10,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.orangehrm.pages.BasePage;
 
+/**
+ * Page Object Model for Edit Employee Page (PIM)
+ * Contains locators and actions related to editing employee personal details
+ */
 public class EditEmpPage extends BasePage {
-    // constructor
+
+    /**
+     * Constructor for EditEmpPage
+     *
+     * @param driver WebDriver instance
+     */
     public EditEmpPage(WebDriver driver) {
         super(driver);
     }
 
-    //locators
+    /**
+     * Locators for Edit Employee page elements
+     */
     private By personalDetailsHeader = By.xpath("//h6[normalize-space()='Personal Details']");
     private By empFullNameTitle = By.xpath("//label[contains(text(),'Employee Full Name')]");
     private By inputEmpFirstName = By.xpath("//input[contains(@class,'orangehrm-firstname')]");
@@ -24,45 +35,74 @@ public class EditEmpPage extends BasePage {
     private By saveBtn = By.xpath("//p[contains(@class,'oxd-text--p')]/following-sibling::button[contains(@type,'submit')]");
     private By successfullySavedMsg = By.xpath("//div/p[contains(normalize-space(),'Successfully Updated')]");
 
-    //-----------wait for element method-----------//
+    /**
+     * Waits until an element is visible on the page
+     *
+     * @param locator element locator
+     * @param timeoutInSeconds wait duration
+     * @return visible WebElement
+     */
     private WebElement waitForElementVisible(By locator, int timeoutInSeconds) {
         return new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds)).until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    //methods
+    /**
+     * Verifies Personal Details header visibility
+     */
     public boolean isPersonalDetailsHeaderDisplayed() {
         return waitForElementVisible(personalDetailsHeader, 10).isDisplayed();
     }
+
+    /**
+     * Verifies Employee Full Name title visibility
+     */
     public boolean isEmpFullNameTitleDisplayed() {
         return waitForElementVisible(empFullNameTitle, 10).isDisplayed();
     }
+
+    /**
+     * Clicks Save button on Edit Employee page
+     */
     public void clickSaveBtn() {
         waitForElementVisible(saveBtn, 10).click();
     }
-    public boolean isSuccessfullySavedMsgVisible(){
+
+    /**
+     * Verifies successfully updated message visibility
+     */
+    public boolean isSuccessfullySavedMsgVisible() {
         return waitForElementVisible(successfullySavedMsg, 30).isDisplayed();
     }
-    
-    public void updateEmpFirstName(String inputfirstname){
+
+    /**
+     * Updates employee first name
+     *
+     * @param inputfirstname new first name value
+     */
+    public void updateEmpFirstName(String inputfirstname) {
         waitForElementVisible(inputEmpFirstName, 5);
-        if(!isElementDisplayed(inputEmpFirstName)){
+
+        if (!isElementDisplayed(inputEmpFirstName)) {
             System.out.println("Employee First Name input not visible!");
             return;
         }
         click(inputEmpFirstName);
-        System.out.println("Employee first name input clicked");
         type(inputEmpFirstName, inputfirstname);
-        System.out.println("Employee first name input filled");
     }
-    public void updateEmpLastName(String inputlastname){
+
+    /**
+     * Updates employee last name
+     *
+     * @param inputlastname new last name value
+     */
+    public void updateEmpLastName(String inputlastname) {
         waitForElementVisible(inputEmpLastName, 5);
-        if(!isElementDisplayed(inputEmpLastName)){
+
+        if (!isElementDisplayed(inputEmpLastName)) {
             System.out.println("Employee Last Name input not visible!");
             return;
         }
         click(inputEmpLastName);
-        System.out.println("Employee last name input clicked");
         type(inputEmpLastName, inputlastname);
-        System.out.println("Employee last name input filled");
     }
 }
